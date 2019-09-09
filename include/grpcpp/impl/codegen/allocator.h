cd3ceeb63225d68ab1de2cfd5a38f85432825e8d
arvids-kokins-bidstack <arvids.kokins@bidstack.com>
2019-09-09 10:54:36 +0300

    remove throws to compile w/o exceptions

--- include/grpcpp/impl/codegen/allocator.h
+++ include/grpcpp/impl/codegen/allocator.h
@@ -21,9 +21,9 @@ struct allocator {
   template <class U>
   constexpr allocator(const allocator<U>&) noexcept {}
   T* allocate(std::size_t n) {
-    if (n > std::size_t(-1) / sizeof(T)) throw std::bad_alloc();
+    if (n > std::size_t(-1) / sizeof(T)) abort();
     if (auto p = static_cast<T*>(gpr_malloc(n * sizeof(T)))) return p;
-    throw std::bad_alloc();
+    abort();
   }
   void deallocate(T* p, std::size_t) noexcept { gpr_free(p); }
 };
